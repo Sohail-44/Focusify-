579d181f0a29c6a77727d7302eb118329a8af15e
MilesMB42 <mmccart3@macalester.edu>
2024-11-18 23:00:54 -0600

    Added objectives interface and goals and tasks classes

---
+++ src/Objectives.java
@@ -0,0 +1,22 @@
+public interface Objectives {
+
+    /* Title Methods */
+    String getTtitle();
+    void setTitle(String newTitle);
+
+    /* Description Methods */
+    String getDescription();
+    void setDescrioption(String newDescription);
+
+    /* Date Methods */
+    //do we need to account for leap years?
+    int getDate();
+    void setDate(int newMonth, int newDay, int newYear); //format of dates could be different?
+
+    /* Other methods */
+    boolean isSoon();
+    boolean isCompleted();
+    void setCompleted( boolean newStatus);
+    
+    
+}
---
+++ src/Tasks.java
@@ -0,0 +1,44 @@
+public class Tasks implements Objectives {
+    private String title;
+    private String description;
+    private int month, day, year;
+    private boolean completed;
+    @Override
+    public String getTtitle() { return title; }
+
+    @Override
+    public void setTitle(String newTitle) { title = newTitle; }
+
+    @Override
+    public String getDescription() { return description; }
+
+    @Override
+    public void setDescrioption(String newDescription) { description = newDescription; }
+
+    @Override
+    public int getDate() {
+        return month, day, year; // what is issue?
+    }
+
+    @Override
+    public void setDate(int newMonth, int newDay, int newYear) {
+       this.month = newMonth;
+       this.day = newDay;
+       this.year = newYear;
+    }
+
+    @Override
+    public boolean isSoon() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isSoon'");
+    }
+
+    @Override
+    public boolean isCompleted() { return completed; }
+
+    @Override
+    public void setCompleted( boolean newStatus) {
+     this.completed = newStatus;
+    }
+    
+}
