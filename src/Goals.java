05d011966b2df5d138ecb5bb21cdcbd39f1f2a82
MilesMB42 <mmccart3@macalester.edu>
2024-11-25 13:24:49 -0600

    Added steps and add, and getter methods

--- src/Goals.java
+++ src/Goals.java
@@ -1,48 +1,44 @@
 
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 public class Goals { 
     private String title;
     private String description;
     private LocalTime time;
     private boolean completed;
+    private ArrayList<String> steps;
     
     public String getTtitle() { return title; }
-
     
     public void setTitle(String newTitle) { title = newTitle; }
 
    
     public String getDescription() { return description; }
 
-   
     public void setDescrioption(String newDescription) { description = newDescription; }
 
     
-    public LocalTime getTime() {
-        return time; 
-    }
+    public LocalTime getTime() { return time; }
     
     public void setTime(int hour, int minute) {
-      time.of(hour, minute);
+      time = LocalTime.of(hour, minute);
     }
-
-    
-
-    
     
     public boolean isSoon() {
-        if ()
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isSoon'");
+       return time.getHour() < LocalTime.now().plusHours(10).getHour();
     }
 
     
     public boolean isCompleted() { return completed; }
 
+    public void setCompleted(boolean newStatus) { this.completed = newStatus; }
+
     
-    public void setCompleted( boolean newStatus) {
-     this.completed = newStatus;
-    }
+    public void addStep(String newStep){ steps.add(newStep); }
+
+    public ArrayList<String> getSteps(){ return steps; }
+
+    public String getStep(int stepNum){ return steps.get(stepNum - 1); }
     
 }
--- src/Tasks.java
+++ src/Tasks.java
@@ -1,4 +1,4 @@
-import java.time.*;
+
 
 
 // public class Tasks {
