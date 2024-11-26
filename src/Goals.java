d18c1c77f11edc5f9f731b07e72f2dd299ba84f2
MilesMB42 <mmccart3@macalester.edu>
2024-11-26 10:18:03 -0600

    more methods to Goals

--- src/Goals.java
+++ src/Goals.java
@@ -1,35 +1,60 @@
 
+import java.sql.Time;
 import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class Goals { 
     private String title;
+    private int timeframe;
     private String description;
     private LocalTime time;
     private boolean completed;
     private ArrayList<String> steps;
     
-    public String getTtitle() { return title; }
+    public String getTitle() { return title; }
     
-    public void setTitle(String newTitle) { title = newTitle; }
+    public void setTitle(String newTitle) { this.title = newTitle; }
 
    
     public String getDescription() { return description; }
 
-    public void setDescrioption(String newDescription) { description = newDescription; }
+    public void setDescrioption(String newDescription) { this.description = newDescription; }
 
     
     public LocalTime getTime() { return time; }
     
     public void setTime(int hour, int minute) {
-      time = LocalTime.of(hour, minute);
+      this.time = LocalTime.of(hour, minute);
+    }
+
+    public int getTimeframe(){ return timeframe; }
+
+    public void setTimeframe(int newTimeframe){ this.timeframe = newTimeframe; }
+    
+    public boolean inTimeframe(int timeRange, int timeframe){
+        return timeframe <= timeRange;
+    }
+
+    /*
+     * gives vaule for how closely the timeframe aligns with the available time range 
+     * lower value means it is closer to fitting into the time range
+     */
+    public int timeframeAlignment(int timeRange, int timeframe){ // make double?
+        return timeRange - timeframe;
     }
     
+
     public boolean isSoon() {
        return time.getHour() < LocalTime.now().plusHours(10).getHour();
     }
-
+    public boolean inGoalTime(int timeRange){
+        if(LocalTime.now().getHour() >= time.getHour() && LocalTime.now().getHour() < (time.getHour() + timeRange)){
+            return true;
+        }
+        else{ return false; }
+    }
     
+
     public boolean isCompleted() { return completed; }
 
     public void setCompleted(boolean newStatus) { this.completed = newStatus; }
